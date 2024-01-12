7290304af5ad8c5ae8361ae6b3ca2120d37576c0
Laurentiu <i478560@fhict.nl>
2024-01-12 09:46:58 +0100

    more unit tests

---
+++ StudentHousing/Tests/AlgorithmTests.cs
@@ -0,0 +1,154 @@
+﻿using DataAcces;
+using Logic.DTO;
+using Logic.Entities;
+using Logic.Interfaces;
+using Logic.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class AlgorithmTests
+    {
+        byte[] testImage;
+        [TestMethod]
+        public void AddHouse_ShoulReturnTrue()
+        {
+            bool success;
+            IHouseRepository houseRepository = new MockAlgorithmRepository();
+            HouseManager houseManager = new HouseManager(houseRepository);
+            HouseDTO newHouse = new HouseDTO();
+            newHouse.HouseID = 1;
+            newHouse.Address = "Address";
+            newHouse.HouseNumber = 1;
+            newHouse.City = "City";
+            newHouse.HouseType = 0;
+            newHouse.Space = 100;
+            newHouse.Furnished = true;
+            newHouse.ContractType = 0;
+            newHouse.Rent = 500;
+            newHouse.Deposit = 100;
+            newHouse.HousePhoto = testImage;
+            newHouse.Status = true;
+
+            if (houseManager.AddHouse(newHouse) == true)
+            {
+                success = true;
+            }
+            else
+            {
+                success = false;
+            }
+            Assert.IsTrue(success);
+        }
+
+        [TestMethod]
+        public void GetHouses_ShouldReturnTrue()
+        {
+            IHouseRepository houseRepository = new MockAlgorithmRepository();
+            HouseManager houseManager = new HouseManager(houseRepository);
+            bool success;
+            if (houseManager.GetAllHouses().Count >= 0)
+            {
+                success = true;
+            }
+            else
+            {
+                success = false;
+            }
+            Assert.IsTrue(success);
+        }
+
+        [TestMethod]
+        public void GetHouseById_ShouldReturnHouse()
+        {
+            IHouseRepository houseRepository = new MockAlgorithmRepository();
+            HouseManager houseManager = new HouseManager(houseRepository);
+            bool success;
+            int id = 1;
+            if (houseManager.GetHouseByID(id) != null)
+            {
+                success = true;
+            }
+            else
+            {
+                success = false;
+            }
+            Assert.IsTrue(success);
+        }
+
+        [TestMethod]
+        public void GetHouseById_ShouldReturnNull()
+        {
+            IHouseRepository houseRepository = new MockAlgorithmRepository();
+            HouseManager houseManager = new HouseManager(houseRepository);
+            int id = 4;
+            Assert.ThrowsException<NullReferenceException>(() => houseManager.GetHouseByID(id));
+        }
+
+        [TestMethod]
+        public void GetRequests_ShouldReturnTrue()
+        {
+            IRequest requestRepository = new MockAlgorithmRepository();
+            RequestManager requestManager = new RequestManager(requestRepository);
+            bool success;
+            if (requestManager.GetAllRequests().Count >= 0)
+            {
+                success = true;
+            }
+            else
+            {
+                success = false;
+            }
+            Assert.IsTrue(success);
+        }
+
+        public List<House> GetAllHouses()
+        {
+            IHouseRepository houseRepository = new MockAlgorithmRepository();
+            HouseManager houseManager = new HouseManager(houseRepository);
+            List<House> houses = new List<House>();
+
+            foreach (HouseDTO h in houseRepository.GetAllHouses())
+            {
+                houses.Add(new House(h));
+            }
+
+            return houses;
+        }
+
+        [TestMethod]
+        public void GetRecommendedHousesByChance_ShouldReturnHouses()
+        {
+            IHouseRepository houseRepository = new MockAlgorithmRepository();
+            IRequest request = new MockAlgorithmRepository();
+            RecommendationManager recommendationManager = new RecommendationManager(houseRepository, request);
+            List<House> houses = GetAllHouses();
+
+            var output = recommendationManager.GetRecommendedHouses("ByChance", houses);
+
+            Assert.AreEqual(3, output.Count);
+            CollectionAssert.Contains(output, houses[0]);
+            CollectionAssert.Contains(output, houses[1]);
+        }
+
+        [TestMethod]
+        public void GetRecommendedHousesByRequest_ShouldReturnHouses()
+        {
+            IHouseRepository houseRepository = new MockAlgorithmRepository();
+            IRequest request = new MockAlgorithmRepository();
+            RecommendationManager recommendationManager = new RecommendationManager(houseRepository, request);
+            List<House> houses = GetAllHouses();
+
+            var output = recommendationManager.GetRecommendedHouses("ByRequest", houses);
+
+            Assert.AreEqual(3, output.Count);
+            Assert.AreEqual(houses[0], output[0]);
+            Assert.AreEqual(houses[1], output[1]);
+        }
+    }
+}
---
+++ StudentHousing/Tests/MockAlgorithmRepository.cs
@@ -0,0 +1,216 @@
+﻿using DataAcces;
+using Logic.DTO;
+using Logic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class MockAlgorithmRepository : IHouseRepository, IRequest
+    {
+        HouseDTO house = new HouseDTO();
+        RequestDTO requestDTO = new RequestDTO();
+        List<HouseDTO> houses;
+        List<RequestDTO> requests;
+        byte[] testImage;
+
+        bool IHouseRepository.AddHouse(HouseDTO houseDTO)
+        {
+            houses = new List<HouseDTO>
+            {
+                houseDTO
+            };
+            if (houses.Count > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool IRequest.AddRequest(RequestDTO requestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<HouseDTO> IHouseRepository.GetAllHouses()
+        {
+            houses = new List<HouseDTO>
+            {
+               new HouseDTO
+                {
+                    HouseID = 1,
+                    Address = "Address",
+                    HouseNumber = 1,
+                    City = "City",
+                    HouseType = 0,
+                    Space = 100,
+                    Furnished = true,
+                    ContractType = 0,
+                    Rent = 500,
+                    Deposit = 100,
+                    HousePhoto = testImage,
+                    Status = true
+               },
+               new HouseDTO
+                {
+                    HouseID = 2,
+                    Address = "Address",
+                    HouseNumber = 1,
+                    City = "City",
+                    HouseType = 0,
+                    Space = 100,
+                    Furnished = true,
+                    ContractType = 0,
+                    Rent = 500,
+                    Deposit = 100,
+                    HousePhoto = testImage,
+                    Status = true
+                },
+               new HouseDTO
+                {
+                    HouseID = 3,
+                    Address = "Address",
+                    HouseNumber = 1,
+                    City = "City",
+                    HouseType = 0,
+                    Space = 100,
+                    Furnished = true,
+                    ContractType = 0,
+                    Rent = 500,
+                    Deposit = 100,
+                    HousePhoto = testImage,
+                    Status = true
+                }
+            };
+            return houses;
+        }
+
+        List<HouseDTO> IHouseRepository.GetAllHousesByStatusAndType(bool Status, int HouseType)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<RequestDTO> IRequest.GetAllRequests()
+        {
+            requests = new List<RequestDTO>
+            {
+               new RequestDTO
+                {
+                    CustomerDTO = new CustomerDTO
+                    {
+                        PersonID = 1,
+                        Email = "test@email.com",
+                        Password = "12345",
+                        IsEmployee = false,
+                        Username = "username",
+                        FirstName = "John",
+                        LastName = "Doe",
+                        Address = "Address",
+                        City = "City",
+                        Country = "Country"
+                    },
+                    HouseDTO = new HouseDTO
+                    {
+                        HouseID = 1,
+                        Address = "Address",
+                        HouseNumber = 1,
+                        City = "City",
+                        HouseType = 0,
+                        Space = 100,
+                        Furnished = true,
+                        ContractType = 0,
+                        Rent = 500,
+                        Deposit = 100,
+                        HousePhoto = testImage,
+                        Status = true
+                    },
+                    Status = 0
+                }
+            };
+            return requests;
+        }
+
+        HouseDTO IHouseRepository.GetHouseByID(int id)
+        {
+            houses = new List<HouseDTO>
+            {
+               new HouseDTO
+                {
+                    HouseID = 1,
+                    Address = "Address",
+                    HouseNumber = 1,
+                    City = "City",
+                    HouseType = 0,
+                    Space = 100,
+                    Furnished = true,
+                    ContractType = 0,
+                    Rent = 500,
+                    Deposit = 100,
+                    HousePhoto = testImage,
+                    Status = true
+               },
+               new HouseDTO
+                {
+                    HouseID = 2,
+                    Address = "Address",
+                    HouseNumber = 1,
+                    City = "City",
+                    HouseType = 0,
+                    Space = 100,
+                    Furnished = true,
+                    ContractType = 0,
+                    Rent = 500,
+                    Deposit = 100,
+                    HousePhoto = testImage,
+                    Status = true
+                },
+               new HouseDTO
+                {
+                    HouseID = 3,
+                    Address = "Address",
+                    HouseNumber = 1,
+                    City = "City",
+                    HouseType = 0,
+                    Space = 100,
+                    Furnished = true,
+                    ContractType = 0,
+                    Rent = 500,
+                    Deposit = 100,
+                    HousePhoto = testImage,
+                    Status = true
+                }
+            };
+            foreach (HouseDTO h in houses)
+            {
+                if (h.HouseID == id)
+                {
+                    return h;
+                }
+            }
+            return null;
+        }
+
+        List<RequestDTO> IRequest.GetRequestsByCustomerID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<RequestDTO> IRequest.GetRequestsByHouseID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IHouseRepository.UpdateHouse(HouseDTO houseDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IRequest.UpdateRequest(RequestDTO requestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
